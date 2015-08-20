bfa13e2c7bce2b1bbddad412919b4955f6383dd6
xuehj <xuehj@nlp.nju.edu.cn>
2015-08-20 10:59:49 +0800

    xhj_08_20

--- oj_leetcode/com/leetcode/failed/Subsets_78.java
+++ oj_leetcode/com/leetcode/failed/Subsets_78.java
@@ -1,5 +1,6 @@
 package com.leetcode.failed;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Subsets_78 {
@@ -11,6 +12,10 @@ public class Subsets_78 {
 }
 class Solution_Subsets_78 {
     public List<List<Integer>> subsets(int[] nums) {
-        
+    	List<Integer> in = new ArrayList<>();
+    	List<List<Integer>> re = new ArrayList<>();
+    	
+    	
+    	return re;
     }
 }
