57a73d5fe35e93b2d6c0665de9ae6cc72a7b6070
xuehj <xuehj@nlp.nju.edu.cn>
2015-08-24 10:53:40 +0800

    xhj_08_24

--- oj_leetcode/com/leetcode/link/Balanced_Binary_Tree_110.java
+++ oj_leetcode/com/leetcode/link/Balanced_Binary_Tree_110.java
@@ -8,9 +8,17 @@ public class Balanced_Binary_Tree_110 {
 
 }
 
-
 class Solution_Balanced_Binary_Tree_110 {
-    public boolean isBalanced(TreeNode root) {
-        
-    }
+	public boolean isBalanced(TreeNode root) {
+		if (root == null)
+			return true;
+		return Math.abs(get_max_depth(root.left) - get_max_depth(root.right)) < 2 && isBalanced(root.left)
+				&& isBalanced(root.right);
+	}
+
+	int get_max_depth(TreeNode root) {
+		if (root == null)
+			return 0;
+		return Math.max(get_max_depth(root.left), get_max_depth(root.right)) + 1;
+	}
 }
