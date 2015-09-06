8921b631e86e55d1a340078e6d778498068c4438
xuehj <xuehj@nlp.nju.edu.cn>
2015-09-06 23:03:07 +0800

    xhjjj

--- oj_lintnode/com/america_leval/Forth_leval.java
+++ oj_lintnode/com/america_leval/Forth_leval.java
@@ -8,6 +8,35 @@ public class Forth_leval {
 
 }
 
+// 更新二进制位
+class Solution_Forth_leval_5 {
+    /**
+     *@param n, m: Two integer
+     *@param i, j: Two bit positions
+     *return: An integer
+     */
+    public int updateBits(int n, int m, int i, int j) {
+    	
+    }
+}
+
+// 尾部的零
+class Solution_Forth_leval_4 {
+    /*
+     * param n: As desciption
+     * return: An integer, denote the number of trailing zeros in n!
+     */
+    public long trailingZeros(long n) {
+    	long ret = 0;
+        while(n!=0){
+            ret += n/5;
+            n /= 5;
+        }
+        return ret;
+    	
+    }
+};
+
 // 不同的路径
 class Solution_Forth_leval_3 {
 	/**
@@ -16,7 +45,20 @@ class Solution_Forth_leval_3 {
 	 * @return an integer
 	 */
 	public int uniquePaths(int m, int n) {
-		
+		int[][] count = new int[m + 1][n + 1];
+		count[0][0] = 0;
+		count[1][1] = 1;
+		for (int i = 1; i <= m; i++)
+			count[i][1] = 1;
+		for (int i = 1; i <= n; i++)
+			count[1][i] = 1;
+		for(int i=2;i<=m;i++){
+			for(int j=2;j<=n;j++){
+				count[i][j] = count[i-1][j]+count[i][j-1];
+			}
+		}
+
+		return count[m][n];
 	}
 }
 
