b2c51b0b58fce3633686a789a6becab2de11cc85
xuehj <xuerenlv@163.com>
2015-09-11 22:01:05 +0800

    xhj_09_11_11

---
+++ oj_leetcode/com/leetcode/failed/Shortest_Palindrome_214.java
@@ -0,0 +1,15 @@
+package com.leetcode.failed;
+
+public class Shortest_Palindrome_214 {
+
+	public static void main(String[] args) {
+
+	}
+
+}
+
+class Solution_Shortest_Palindrome_214 {
+	public String shortestPalindrome(String s) {
+
+	}
+}
