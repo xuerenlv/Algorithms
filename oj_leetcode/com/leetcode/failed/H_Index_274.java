1089061e0c55f56f66056d763c5066e501f32d14
xuehj <xuerenlv@163.com>
2015-09-20 16:58:43 +0800

    xhj_09_20_12

--- oj_leetcode/com/leetcode/failed/H_Index_274.java
+++ oj_leetcode/com/leetcode/failed/H_Index_274.java
@@ -1,15 +1,34 @@
 package com.leetcode.failed;
 
+import java.util.Arrays;
+
 public class H_Index_274 {
 
 	public static void main(String[] args) {
-
+		int[] nums = { 3, 0, 6, 1, 5 };
+		System.out.println(new Solution_H_Index_274().hIndex(nums));
 	}
 
 }
 
 class Solution_H_Index_274 {
 	public int hIndex(int[] citations) {
+		if (citations == null || citations.length == 0)
+			return 0;
+		Arrays.sort(citations);
+		int[] temp = new int[citations.length];
 
+		int index = 0;
+		for (int i = citations.length - 1; i >= 0; i--, index++) {
+			temp[index] = citations[i];
+		}
+
+		for (int i = 1; i <= citations.length; i++) {
+			if (i > temp[i - 1]) {
+				return i - 1;
+			}
+		}
+
+		return temp.length;
 	}
 }
---
+++ oj_leetcode/com/leetcode/failed/H_Index_II_275.java
@@ -0,0 +1,16 @@
+package com.leetcode.failed;
+
+public class H_Index_II_275 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+class Solution_H_Index_II_275 {
+	public int hIndex(int[] citations) {
+
+	}
+}
