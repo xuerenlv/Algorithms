0dae196b975b4200d90463580b558f8c74dba849
xuehj <xuerenlv@163.com>
2015-09-21 16:22:37 +0800

    xhj_09_20_121

--- oj_leetcode/com/leetcode/failed/Expression_Add_Operators_282.java
+++ oj_leetcode/com/leetcode/failed/Expression_Add_Operators_282.java
@@ -1,5 +1,6 @@
 package com.leetcode.failed;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Expression_Add_Operators_282 {
@@ -10,9 +11,32 @@ public class Expression_Add_Operators_282 {
 
 }
 
-
 class Solution_Expression_Add_Operators_282 {
-    public List<String> addOperators(String num, int target) {
-        
-    }
+	public List<String> addOperators(String num, int target) {
+		List<String> res = new ArrayList<>();
+		addOperatorsDFS(num, target, 0, 0, "", res);
+		return res;
+	}
+
+	void addOperatorsDFS(String num, int target, int diff, int curNum, String out, List<String> res) {
+		if (num.length() == 0 && curNum == target) {
+			res.add(out);
+		}
+		for (int i = 1; i <= num.length(); ++i) {
+			String cur = num.substring(0, i);
+			if (cur.length() > 1 && cur.charAt(0) == '0')
+				return;
+			String next = num.substring(i);
+			if (out.length() > 0) {
+				addOperatorsDFS(next, target, Integer.parseInt(cur), curNum + Integer.parseInt(cur), out + "+" + cur,
+						res);
+				addOperatorsDFS(next, target, -Integer.parseInt(cur), curNum - Integer.parseInt(cur), out + "-" + cur,
+						res);
+				addOperatorsDFS(next, target, diff * Integer.parseInt(cur),
+						(curNum - diff) + diff * Integer.parseInt(cur), out + "*" + cur, res);
+			} else {
+				addOperatorsDFS(next, target, Integer.parseInt(cur), Integer.parseInt(cur), cur, res);
+			}
+		}
+	}
 }
