82fc305bd690e1f1c128cd1a4a099cbade325794
xuehj <xuehj@nlp.nju.edu.cn>
2015-09-27 23:23:11 +0800

    xhj_09_27_110

--- oj_leetcode_second/com/string_manu/ZigZag_Conversion_6.java
+++ oj_leetcode_second/com/string_manu/ZigZag_Conversion_6.java
@@ -3,7 +3,8 @@ package com.string_manu;
 public class ZigZag_Conversion_6 {
 
 	public static void main(String[] args) {
-//		System.out.println(new Solution_ZigZag_Conversion_6().convert("PAYPALISHIRING", 3));
+		// System.out.println(new
+		// Solution_ZigZag_Conversion_6().convert("PAYPALISHIRING", 3));
 		System.out.println(new Solution_ZigZag_Conversion_6().convert("A", 1));
 	}
 
@@ -14,15 +15,15 @@ class Solution_ZigZag_Conversion_6 {
 		String[] h_strs = new String[numRows];
 		for (int i = 0; i < h_strs.length; i++)
 			h_strs[i] = "";
-		
-		if(numRows<3){
-			if(numRows==1)
+
+		if (numRows < 3) {
+			if (numRows == 1)
 				return s;
-			if(numRows==2){
-				int j=0;
-				for(int i=0;i<s.length();i++){
-					j=i%2;
-					h_strs[j]+=s.charAt(i);
+			if (numRows == 2) {
+				int j = 0;
+				for (int i = 0; i < s.length(); i++) {
+					j = i % 2;
+					h_strs[j] += s.charAt(i);
 				}
 				String re = "";
 				for (int i = 0; i < h_strs.length; i++) {
@@ -31,17 +32,15 @@ class Solution_ZigZag_Conversion_6 {
 				return re;
 			}
 		}
-		
+
 		int j;
 		int i = 0;
-		int bian=0;
 		while (i < s.length()) {
-			bian =i/(2*numRows-2);			//取整，得到是第几遍
-			int jishu =i-bian*(2*numRows-2);	
-			if(jishu > numRows-1){
-				j=numRows-1 - (jishu-(numRows-1));
-			}else{
-				j = jishu % numRows;	
+			int jishu = i % (2 * numRows - 2);
+			if (jishu > numRows - 1) {
+				j = 2 * numRows - 2 - jishu;
+			} else {
+				j = jishu % numRows;
 			}
 			h_strs[j] = h_strs[j] + s.charAt(i);
 			i++;
