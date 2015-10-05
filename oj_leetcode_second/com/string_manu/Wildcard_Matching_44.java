e06ce4dda913ef80fe77a943118ae71ce17eda29
xuehj <xuerenlv@163.com>
2015-10-05 21:37:42 +0800

    xhj_10_dp_1

--- oj_leetcode_second/com/string_manu/Wildcard_Matching_44.java
+++ oj_leetcode_second/com/string_manu/Wildcard_Matching_44.java
@@ -1,54 +1,74 @@
 package com.string_manu;
 
-import java.util.ArrayList;
-
 public class Wildcard_Matching_44 {
 
 	public static void main(String[] args) {
-		// System.out.println(new
-		// Solution_Wildcard_Matching_44().isMatch("abefcdgiescdfimde",
-		// "ab*cd?i*de"));
+		String s = "babbbbaabababaabbababaababaabbaabababbaaababbababaaaaaabbabaaaabababbabbababbbaaaababbbabbbbbbbbbbaabbb";
+		String p = "b**bb**a**bba*b**a*bbb**aba***babbb*aa****aabb*bbb***a";
 
+		System.out.println(new Solution_Wildcard_Matching_44().isMatch_dp_1(s, p));
+		System.out.println(new Solution_Wildcard_Matching_44().isMatch_dp_2("aa", "*"));
+		// System.out.println(new Solution_Wildcard_Matching_44().isMatch(s,
+		// p));
 	}
 
 }
 
 class Solution_Wildcard_Matching_44 {
-	public boolean isMatch_tan_xin(String s, String p) {
-		ArrayList<String> p_pattern = new ArrayList<>();
-		ArrayList<String> rs_li = new ArrayList<>();
 
-		int index_s = 0;
-		int index_p = 0;
-		while (index_s < s.length() && index_p < p.length()) {
-			if (s.charAt(index_s) == p.charAt(index_p) || p.charAt(index_p) == '?') {
-				index_s++;
-				index_p++;
-				continue;
-			}
-			if (p.charAt(index_p) == '*') {
-				p_pattern.add(p.substring(index_p + 1));
-				index_p++;
-				rs_li.add(s);
-				continue;
-			}
-			if (p_pattern.size() != 0) {
-				p = p_pattern.get(0);
-				index_p = 0;
-				s = rs_li.get(0);
-				index_s++;
-				continue;
-			}
-			return false;
-
+	// 这个是根据人家dp的思想，自己做的
+	public boolean isMatch_dp_2(String s, String p) {
+		boolean[][] match = new boolean[s.length() + 1][p.length() + 1];
+		match[0][0] = true;
+		for (int i = 1; i < s.length(); i++) {
+			match[i][0] = false;
 		}
-		while (p.charAt(index_p) == '*')
-			index_p++;
-		return index_p == '\0';
+		for (int i = 1; i <= p.length(); i++) {
+			if (p.charAt(i - 1) == '*') {
+				match[0][i] = match[0][i - 1];
+			}
+		}
+
+		for (int i = 1; i <= s.length(); i++) {
+			for (int j = 1; j <= p.length(); j++) {
+				if (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '?')
+					match[i][j] = match[i - 1][j - 1];
+				else if (p.charAt(j - 1) == '*')
+					match[i][j] = match[i][j - 1] || match[i - 1][j];
+				else
+					match[i][j] = false;
+			}
+		}
+		return match[s.length()][p.length()];
+	}
+
+	// DP , from discuss
+	public boolean isMatch_dp_1(String s, String p) {
+		boolean[][] match = new boolean[s.length() + 1][p.length() + 1];
+		match[s.length()][p.length()] = true;
+		for (int i = p.length() - 1; i >= 0; i--) {
+			if (p.charAt(i) != '*')
+				break;
+			else
+				match[s.length()][i] = true;
+		}
+		for (int i = s.length() - 1; i >= 0; i--) {
+			for (int j = p.length() - 1; j >= 0; j--) {
+				if (s.charAt(i) == p.charAt(j) || p.charAt(j) == '?')
+					match[i][j] = match[i + 1][j + 1];
+				else if (p.charAt(j) == '*')
+					match[i][j] = match[i + 1][j] || match[i][j + 1];
+				else
+					match[i][j] = false;
+			}
+		}
+		return match[0][0];
 	}
 
 	// Time Limit Exceeded
 	// 递归的版本，耗时太大
+	// "babbbbaabababaabbababaababaabbaabababbaaababbababaaaaaabbabaaaabababbabbababbbaaaababbbabbbbbbbbbbaabbb"
+	// "b**bb**a**bba*b**a*bbb**aba***babbb*aa****aabb*bbb***a"
 	public boolean isMatch(String s, String p) {
 		if (p.length() == 0)
 			return s.length() == 0;
