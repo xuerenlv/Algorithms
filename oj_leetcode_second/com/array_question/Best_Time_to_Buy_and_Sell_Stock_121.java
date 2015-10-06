f4fd92813360bc466e5d4cef3ef7ce3c84cec230
xuehj <xuerenlv@163.com>
2015-10-06 15:26:20 +0800

    xhj_10_06_03

--- oj_leetcode_second/com/array_question/Best_Time_to_Buy_and_Sell_Stock_121.java
+++ oj_leetcode_second/com/array_question/Best_Time_to_Buy_and_Sell_Stock_121.java
@@ -1,4 +1,4 @@
-package com.leetcode;
+package com.array_question;
 
 public class Best_Time_to_Buy_and_Sell_Stock_121 {
 
@@ -14,10 +14,10 @@ class Solution_Best_Time_to_Buy_and_Sell_Stock_121 {
 		if (len == 0)
 			return 0;
 		
-		int max_price=0;
+		int max_price=prices[len-1];
 		int re=0;
 		// 从后向前遍历，因为肯定要在后面的最高值卖，才可以获取最大利润
-		for(int i=len-1;i>=0;i--){
+		for(int i=len-2;i>=0;i--){
 			max_price=Math.max(max_price, prices[i]);
 			re=Math.max(re, max_price-prices[i]);
 		}
