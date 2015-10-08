14594362a30a09cd7b85f9e48b2c74c15e4c4551
xuerenlv <xuerenlv@gmail.com>
2015-10-08 21:05:54 +0800

    xhj_10_08_008

--- oj_leetcode_second/com/string_manu/Shortest_Palindrome_214.java
+++ oj_leetcode_second/com/string_manu/Shortest_Palindrome_214.java
@@ -9,6 +9,21 @@ public class Shortest_Palindrome_214 {
 }
 
 class Solution_Shortest_Palindrome_214 {
+
+	public String shortestPalindrome__re(String s) {
+		int j = 0;
+		for (int i = s.length() - 1; i >= 0; i--) {
+			if (s.charAt(i) == s.charAt(j)) {
+				j += 1;
+			}
+		}
+		if (j == s.length()) {
+			return s;
+		}
+		String suffix = s.substring(j);
+		return new StringBuffer(suffix).reverse().toString() + shortestPalindrome(s.substring(0, j)) + suffix;
+	}
+
 	public String shortestPalindrome(String s) {
 		if (s.length() <= 1)
 			return s;
