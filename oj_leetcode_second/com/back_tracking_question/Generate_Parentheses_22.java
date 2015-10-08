905312ee605de00bee6150ca0d607d6c7305783f
xuehj <xuerenlv@163.com>
2015-10-08 15:43:21 +0800

    xhj_10_08_001

--- oj_leetcode_second/com/back_tracking_question/Generate_Parentheses_22.java
+++ oj_leetcode_second/com/back_tracking_question/Generate_Parentheses_22.java
@@ -1,4 +1,4 @@
-package com.leetcode;
+package com.back_tracking_question;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,6 +15,25 @@ public class Generate_Parentheses_22 {
 }
 
 class Solution_Generate_Parentheses_22 {
+	// 不用递归
+	public List<String> generateParenthesis_2(int n) {
+		List<String>[] temp = new ArrayList[n + 1];
+		temp[0] = new ArrayList<>();
+		temp[0].add("");
+
+		for (int i = 1; i <= n; i++) {
+			temp[i] = new ArrayList<>();
+			for (int j = 0; j < i; j++) {
+				for (String t1 : temp[j])
+					for (String t2 : temp[i - 1 - j])
+						temp[i].add("(" + t1 + ")" + t2);
+			}
+		}
+
+		return temp[n];
+
+	}
+
 	public List<String> generateParenthesis(int n) {
 		List<String> re = new ArrayList<>();
 		gen_pairs(re, n, 0, 0, "");
