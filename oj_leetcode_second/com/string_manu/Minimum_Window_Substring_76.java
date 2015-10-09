aebae607584282ed4b8b619700a89e21c7535680
xuerenlv <xuerenlv@gmail.com>
2015-10-09 20:56:36 +0800

    xhj——10——09——2

--- oj_leetcode_second/com/string_manu/Minimum_Window_Substring_76.java
+++ oj_leetcode_second/com/string_manu/Minimum_Window_Substring_76.java
@@ -12,6 +12,47 @@ public class Minimum_Window_Substring_76 {
 
 class Solution_Minimum_Window_Substring_76 {
 
+	// 这里也是使用两个数组，说明这种方式很好，也是双指针
+	// 但是数组纪录的数据不一样，很有趣
+	public String minWindow_2(String S, String T) {
+		if (S == null || S.isEmpty() || T == null || T.isEmpty())
+			return "";
+		int i = 0, j = 0;
+		int[] Tmap = new int[256];
+		int[] Smap = new int[256];
+		for (int k = 0; k < T.length(); k++) {
+			Tmap[T.charAt(k)]++;
+		}
+		int found = 0;
+		int length = Integer.MAX_VALUE;
+		String res = "";
+		while (j < S.length()) {
+			if (found < T.length()) {
+				if (Tmap[S.charAt(j)] > 0) {
+					Smap[S.charAt(j)]++;
+					if (Smap[S.charAt(j)] <= Tmap[S.charAt(j)]) {
+						found++;
+					}
+				}
+				j++;
+			}
+			while (found == T.length()) {
+				if (j - i < length) {
+					length = j - i;
+					res = S.substring(i, j);
+				}
+				if (Tmap[S.charAt(i)] > 0) {
+					Smap[S.charAt(i)]--;
+					if (Smap[S.charAt(i)] < Tmap[S.charAt(i)]) {
+						found--;
+					}
+				}
+				i++;
+			}
+		}
+		return res;
+	}
+
 	// 写的也是很优雅，用两个 数组 来纪录一个字符串的特征
 	// 一个纪录有那些字符出现，另一个纪录字符出现了几次
 	// 还有一点就是双指针，也很美
