4820374d6d78500fe226f7c50452ff889e6cd7fd
xuehj <xuehj@nlp.nju.edu.cn>
2015-10-20 22:29:06 +0800

    vv

--- src/com/run_te/t3.java
+++ src/com/run_te/t3.java
@@ -5,6 +5,7 @@ public class t3 {
 	public static void main(String[] args) {
 		System.out.println("abc".substring(0));
 		System.out.println("abc".substring(1));
+		System.out.println("hello world 薛洪见!");
 	}
 
 }
