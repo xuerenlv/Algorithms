67ec5b28f04b4027435fca0710c0981cf608fba8
xuerenlv <xuerenlv@gmail.com>
2015-11-04 22:11:57 +0800

    对于链表的旋转，双指针也是可以解决问题

--- oj_leetcode_second/com/link_question/Rotate_List_61.java
+++ oj_leetcode_second/com/link_question/Rotate_List_61.java
@@ -1,4 +1,4 @@
-package com.leetcode.link;
+package com.link_question;
 
 public class Rotate_List_61 {
 
@@ -10,9 +10,9 @@ public class Rotate_List_61 {
 		p.next = new ListNode(3);
 		p = p.next;
 		p.next = new ListNode(4);
-		
+
 		head = new Solution_Rotate_List_61().rotateRight(head, 2);
-		
+
 		for (p = head; p != null; p = p.next) {
 			System.out.print(p.val + "   ");
 		}
@@ -23,6 +23,45 @@ public class Rotate_List_61 {
 }
 
 class Solution_Rotate_List_61 {
+
+	// 使用 双指针
+	public ListNode rotateRight_2(ListNode head, int k) {
+		if (k == 0 || head == null)
+			return head;
+
+		ListNode p = head;
+		ListNode q = head, s;
+
+		int len = 1;
+		while (q.next != null) {
+			len++;
+			q = q.next;
+		}
+
+		if (len == 1)
+			return head;
+		if (k >= len)
+			k = k % len;
+		if (k == 0)
+			return head;
+		// 双指针
+		q = head;
+		int gap = 0;
+		while (gap < k) {
+			q = q.next;
+			gap++;
+		}
+		while (q.next != null) {
+			p = p.next;
+			q = q.next;
+		}
+		s = p.next;
+		p.next = null;
+		q.next = head;
+		return s;
+	}
+
+	// 模拟 ， 先是尾插法，然后是头插法（逆序）
 	public ListNode rotateRight(ListNode head, int k) {
 		if (k == 0 || head == null)
 			return head;
@@ -39,10 +78,10 @@ class Solution_Rotate_List_61 {
 			tail = tail.next;
 		}
 
-		if(len==1)
+		if (len == 1)
 			return head;
 		if (k >= len)
-			k=k%len;
+			k = k % len;
 
 		int left = len - k;
 		while (left > 0) {
@@ -53,7 +92,7 @@ class Solution_Rotate_List_61 {
 			new_tail = s;
 			left--;
 		}
-		if(p!=null){
+		if (p != null) {
 			new_tail = new_head;
 			tail.next = new_tail.next;
 			new_tail.next = p;
