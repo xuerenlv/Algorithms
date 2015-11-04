e04ef1e4e92f4170ba538a33e0c446b4f7aa45d7
xuerenlv <xuerenlv@gmail.com>
2015-11-04 20:22:05 +0800

    spiral matrix

--- oj_leetcode_second/com/array_question/Spiral_Matrix_II_59.java
+++ oj_leetcode_second/com/array_question/Spiral_Matrix_II_59.java
@@ -1,4 +1,4 @@
-package com.leetcode;
+package com.array_question;
 
 import java.util.Arrays;
 
@@ -14,6 +14,53 @@ public class Spiral_Matrix_II_59 {
 }
 
 class Solution_Spiral_Matrix_II_59 {
+	// 一个是基于纪录访问
+	// 一个是固定开始与结束，都是模拟这个过程
+
+	public int[][] generateMatri_2(int n) {
+		int[][] mat = new int[n][n];
+		if (n == 0)
+			return mat;
+
+		int rowBegin = 0;
+		int rowEnd = mat.length - 1;
+		int colBegin = 0;
+		int colEnd = mat[0].length - 1;
+
+		int tot = 1;
+		while (rowBegin <= rowEnd && colBegin <= colEnd) {
+			// Traverse Right
+			for (int j = colBegin; j <= colEnd; j++) {
+				mat[rowBegin][j] = tot++;
+			}
+			rowBegin++;
+
+			// Traverse Down
+			for (int j = rowBegin; j <= rowEnd; j++) {
+				mat[j][colEnd] = tot++;
+			}
+			colEnd--;
+
+			if (rowBegin <= rowEnd) {
+				// Traverse Left
+				for (int j = colEnd; j >= colBegin; j--) {
+					mat[rowEnd][j] = tot++;
+				}
+			}
+			rowEnd--;
+
+			if (colBegin <= colEnd) {
+				// Traver Up
+				for (int j = rowEnd; j >= rowBegin; j--) {
+					mat[j][colBegin] = tot++;
+				}
+			}
+			colBegin++;
+		}
+
+		return mat;
+	}
+
 	public int[][] generateMatrix(int n) {
 		int[][] mat = new int[n][n];
 		if (n == 0)
