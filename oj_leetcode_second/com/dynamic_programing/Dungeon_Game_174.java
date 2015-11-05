088844addf8cc7300aa1a58535bc0bef435c81e0
xuerenlv <xuerenlv@gmail.com>
2015-11-05 15:40:19 +0800

    graph dynamic programing

--- oj_leetcode_second/com/dynamic_programing/Dungeon_Game_174.java
+++ oj_leetcode_second/com/dynamic_programing/Dungeon_Game_174.java
@@ -1,4 +1,4 @@
-package com.leetcode.graph;
+package com.dynamic_programing;
 
 public class Dungeon_Game_174 {
 
@@ -12,12 +12,41 @@ public class Dungeon_Game_174 {
 
 class Solution_Dungeon_Game_174 {
 
+	// use the original space
+	public int calculateMinimumHP_2(int[][] dungeon) {
+		// return 0 if dungeon does not exist
+		if (dungeon == null || dungeon.length == 0 || dungeon[0].length == 0)
+			return 0;
+		int m = dungeon.length - 1, n = dungeon[0].length - 1;
+		// make sure alive after rescue
+		dungeon[m][n] = setHp(1 - dungeon[m][n]);
+
+		// fill bottom row and right-most column
+		for (int i = m - 1; i >= 0; i--)
+			dungeon[i][n] = setHp(dungeon[i + 1][n] - dungeon[i][n]);
+		for (int j = n - 1; j >= 0; j--)
+			dungeon[m][j] = setHp(dungeon[m][j + 1] - dungeon[m][j]);
+
+		// fill the rest
+		for (int i = m - 1; i >= 0; i--)
+			for (int j = n - 1; j >= 0; j--)
+				// pick minimum hp needed after this cell
+				dungeon[i][j] = setHp(Math.min(dungeon[i + 1][j], dungeon[i][j + 1]) - dungeon[i][j]);
+
+		return dungeon[0][0];
+	}
+
+	// if needed Hp is negative set hp to 1, otherwise positive
+	private int setHp(int hp) {
+		return hp <= 0 ? 1 : hp;
+	}
+
 	public int calculateMinimumHP(int[][] dungeon) {
 		// 二维空间DP问题
 		int m = dungeon.length;
 		int n = dungeon[0].length;
-		// 0225
-		int[][] dp = new int[m][n];// dp[i][j]表示从(i,j)到目的地(m-1,n-1)需要的最小生命值
+		// dp[i][j]表示从(i,j)到目的地(m-1,n-1)需要的最小生命值
+		int[][] dp = new int[m][n];
 		// 初始化 操作数据（change数据）
 		dp[m - 1][n - 1] = Math.max(0 - dungeon[m - 1][n - 1], 0);
 		for (int i = m - 2; i >= 0; i--) {
@@ -30,9 +59,11 @@ class Solution_Dungeon_Game_174 {
 		// dp方程为 dp[i][j] = min(dp[i][j+1], dp[i+1][j]) - dungeon[i][j] 再和0取最大
 		for (int i = m - 2; i >= 0; i--) {
 			for (int j = n - 2; j >= 0; j--) {
+				// at least 0 ，因为王子的生命值不可以低于0，低于0，早就挂了
 				dp[i][j] = Math.max(Math.min(dp[i][j + 1], dp[i + 1][j]) - dungeon[i][j], 0);
 			}
 		}
+		// 因为等于零就挂了，所以要加1
 		return dp[0][0] + 1;
 	}
 
@@ -50,7 +81,7 @@ class Solution_Dungeon_Game_174 {
 		if (h_start == h_end && c_start == c_end)
 			return grid[h_end][c_end];
 
-		return Math.min(grid[h_start][c_start] + calc_min(grid, h_start, c_start + 1, h_end, c_end), grid[h_start][c_start]
-				+ calc_min(grid, h_start + 1, c_start, h_end, c_end));
+		return Math.min(grid[h_start][c_start] + calc_min(grid, h_start, c_start + 1, h_end, c_end),
+				grid[h_start][c_start] + calc_min(grid, h_start + 1, c_start, h_end, c_end));
 	}
 }
