271236f85370301dbf5ae318c111cf83d21dac40
xuerenlv <xuerenlv@gmail.com>
2015-11-10 22:23:52 +0800

    层序遍历 very good

--- oj_leetcode_second/com/binary_tree/Minimum_Depth_of_Binary_Tree_111.java
+++ oj_leetcode_second/com/binary_tree/Minimum_Depth_of_Binary_Tree_111.java
@@ -1,4 +1,4 @@
-package com.leetcode.link_2;
+package com.binary_tree;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
@@ -12,6 +12,26 @@ public class Minimum_Depth_of_Binary_Tree_111 {
 }
 
 class Solution_Minimum_Depth_of_Binary_Tree_111 {
+	// 递归，易于想到一点
+	public static int minDepth_3(TreeNode root) {
+		if (root == null)
+			return 0;
+		if (root.left == null)
+			return minDepth_3(root.right) + 1;
+		if (root.right == null)
+			return minDepth_3(root.left) + 1;
+		return Math.min(minDepth_3(root.left), minDepth_3(root.right)) + 1;
+	}
+
+	// 递归用法，很精巧
+	public int minDepth_2(TreeNode root) {
+		if (root == null)
+			return 0;
+		int left = minDepth(root.left);
+		int right = minDepth(root.right);
+		return (left == 0 || right == 0) ? left + right + 1 : Math.min(left, right) + 1;
+	}
+
 	// 活用二叉树的层序遍历
 	public int minDepth(TreeNode root) {
 		if (root == null)
@@ -29,9 +49,9 @@ class Solution_Minimum_Depth_of_Binary_Tree_111 {
 			while (cur < count) {
 				TreeNode p = qu.getFirst();
 				qu.pop();
-				
-				if(p.left == null && p.right == null){
-				    return ceng;
+
+				if (p.left == null && p.right == null) {
+					return ceng;
 				}
 
 				if (p.left != null) {
