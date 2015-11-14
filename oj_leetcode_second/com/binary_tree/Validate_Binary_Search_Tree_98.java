e0af1a9ebdc0372d2f2bf0d89a75964c84a1f7a6
xuerenlv <xuerenlv@gmail.com>
2015-11-14 19:17:18 +0800

    binary search tree

--- oj_leetcode_second/com/binary_tree/Validate_Binary_Search_Tree_98.java
+++ oj_leetcode_second/com/binary_tree/Validate_Binary_Search_Tree_98.java
@@ -1,6 +1,8 @@
-package com.leetcode.link;
+package com.binary_tree;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
 
 public class Validate_Binary_Search_Tree_98 {
 
@@ -11,6 +13,41 @@ public class Validate_Binary_Search_Tree_98 {
 }
 
 class Solution_Validate_Binary_Search_Tree_98 {
+
+	// 中序遍历非递归
+	void bin_inorder_with_iterative(TreeNode root, List<Integer> re) {
+		// 中序遍历非递归，使用 栈
+		Stack<TreeNode> stack = new Stack<TreeNode>();
+
+		TreeNode p = root;
+		while (p != null || !stack.isEmpty()) {
+			while (p != null) { // 左分支全部进栈
+				stack.push(p);
+				p = p.left;
+			}
+			if (!stack.isEmpty()) {
+				p = stack.pop();
+				re.add(p.val);
+				p = p.right;// 处理过根节点之后，处理右分支
+			}
+		}
+	}
+	// *****************************************************************************
+
+	// 非常好，这种解法很清晰
+	public boolean isValidBST(TreeNode root) {
+		return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+	}
+
+	public boolean isValidBST(TreeNode root, long minVal, long maxVal) {
+		if (root == null)
+			return true;
+		if (root.val >= maxVal || root.val <= minVal)
+			return false;
+		return isValidBST(root.left, minVal, root.val) && isValidBST(root.right, root.val, maxVal);
+	}
+
+	// **************************************************************************
 	// 二叉查找树的中序遍历序列，是递增有序的
 	public boolean isValidBST_accepted(TreeNode root) {
 		ArrayList<Integer> con_pri = new ArrayList<Integer>();
@@ -25,16 +62,16 @@ class Solution_Validate_Binary_Search_Tree_98 {
 		return flag;
 	}
 
+	// 递归中序遍历
 	void priview(TreeNode root, ArrayList<Integer> container) {
 		if (root == null)
 			return;
 		priview(root.left, container);
-
 		container.add(root.val);
-
 		priview(root.right, container);
 	}
 
+	// ***************************************************************************
 	// 错误，是不是bst，还与整体有关
 	public boolean isValidBST_wrong(TreeNode root) {
 		if (root == null)
